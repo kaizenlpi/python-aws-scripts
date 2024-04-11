e5e40cd3e5c3235644a24a8d744696cdfc7d96c5
Matt Brady <kaizenlpi@users.noreply.github.com>
2024-04-11 14:51:21 -0400

    Update delete-ebs-snapshots.py

--- delete-ebs-snapshots.py
+++ delete-ebs-snapshots.py
@@ -1,29 +1,21 @@
+# Note: except captures the 2c2.delete(snapshot) command STDERR in the variable "e". It prints the "e" variable, then continues iterating through the list of values. 
+# list of values came from excel spreadsheet see line 10
+
 import boto3 
 import pprint
+import logging
 from openpyxl import load_workbook
 AWS_REGION = "ap-southeast-2"
 ec2 = boto3.client('ec2', region_name = AWS_REGION)
-
-wb = load_workbook("myworkbook".xlsx")  
+wb = load_workbook("CREPCE_GovCloud_Snapshots_040824-1.xlsx")  
 ws = wb['Sheet1']
-column = ws['D'][0:3089]  # work book has 3089 rows to review
-column_list = [column[x].value for x in range(len(column))]  
-
-#snapshot_string =" ".join(map(str,column_list)).split(",") #USE MAP TO CONVERT LIST TO STRING # USE SPLIT METHOD TO DIVIDE STRINGS AS MULTIPLE SUBSSTRINGS # 4/11 Unneeded
-pprint.pprint(column_list)
-
-print(f'The snapshot for loop is starting')
+column = ws['D'][1:3089]  
+column_list = [column[x].value for x in range(len(column))]
 for snap in column_list:
-    print(f"Snapshot {snap} are deleting")
-    if ec2.delete_snapshot(SnapshotId=snap) and SnapshotId.InUse == "true":
-        print(f'Snapshots are in use by different AMIs')
-    else: 
-        print(f"Snapshot {snap} have been deleted")
-
-
-'''NOTE: 
-SCRIPT WORKED
-OUTPUT RETURNED An error occurred (InvalidSnapshot.InUse) when calling the DeleteSnapshot operation: The snapshot snap-09d9a33133b8dcff8 is currently in use by ami-092b734a6f7e56317.
-
-IMPROVEMENT OPPORUNITY: IF STATEMENT NOT WORKING
-'''
+    #print(f"Try deleting Snapshot {snap}")
+    try:
+        ec2.delete_snapshot(SnapshotId=snap)
+    except Exception as e:
+        print(e)
+        continue
+    print("Loop has finished")
